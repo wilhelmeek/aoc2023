3740053d0f302d72ff8fb87064701240e82d8bcf
wilhelm <helm@hey.com>
2023-12-03 15:43:18 +1100

    s2

---
+++ internal/day_one/day_two/day_two.go
@@ -0,0 +1,11 @@
+package daytwo
+
+type DayTwo struct{}
+
+func (d DayTwo) Name() string {
+	return "Day Two"
+}
+
+func (d DayTwo) Solve() error {
+	return nil
+}
--- runner.go
+++ runner.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	dayone "github.com/wilhelmeek/aoc2023/internal/day_one"
+	daytwo "github.com/wilhelmeek/aoc2023/internal/day_one/day_two"
 	"github.com/wilhelmeek/aoc2023/internal/solver"
 )
 
 func main() {
 	solvers := []solver.Solver{
 		dayone.DayOne{},
+		daytwo.DayTwo{},
 	}
 
 	for _, s := range solvers {
+		fmt.Println(fmt.Sprintf("Solving %q", s.Name()))
 		if err := s.Solve(); err != nil {
 			log.Fatalf("Failed to solve %q: %s", s.Name(), err)
 		}
