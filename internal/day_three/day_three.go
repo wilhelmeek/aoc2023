15327565d3be3e1a9e6dc9ea6333ba7309146b6b
wilhelm <helm@hey.com>
2023-12-09 20:32:08 +1100

    gross as shit

--- internal/day_three/day_three.go
+++ internal/day_three/day_three.go
@@ -16,75 +16,117 @@ func (sl Solution) Name() string {
 
 type row []rune
 type val rune
+type gear struct{ nums map[int]int }
 
-func (v val) number() bool {
+func (g *gear) add(n int) {
+	g.nums[n]++
+}
+
+func (v val) num() bool {
 	return unicode.IsNumber(rune(v))
 }
 
-func (v val) symbol() bool {
-	if v == 0 {
-		return false
-	}
-	if v == '.' {
-		return false
-	}
-	return !unicode.IsDigit(rune(v))
+func (v val) sym() bool {
+	return v != 0 &&
+		v != '.' &&
+		!unicode.IsDigit(rune(v))
+}
+
+func (v val) gear() bool {
+	return v == '*'
 }
 
 type schematic struct {
 	lbX, lbY, ubX, ubY int
 	rows               []row
+	gears              map[string]*gear
+	parts              []int
 }
 
 func newSchematic(rows []row) schematic {
-	return schematic{
-		lbX:  0,
-		lbY:  0,
-		ubX:  len(rows[0]) - 1,
-		ubY:  len(rows) - 1,
-		rows: rows,
+	s := schematic{
+		lbX:   0,
+		lbY:   0,
+		ubX:   len(rows[0]) - 1,
+		ubY:   len(rows) - 1,
+		rows:  rows,
+		gears: map[string]*gear{},
+		parts: []int{},
 	}
-}
-
-func (s schematic) symbolAdj(x, y int) bool {
-	return false ||
-		s.at(x-1, y).symbol() ||
-		s.at(x+1, y).symbol() ||
-		s.at(x, y-1).symbol() ||
-		s.at(x, y+1).symbol() ||
-		s.at(x+1, y+1).symbol() ||
-		s.at(x+1, y-1).symbol() ||
-		s.at(x-1, y+1).symbol() ||
-		s.at(x-1, y-1).symbol()
-}
-
-func (s schematic) parts() []string {
-	var parts []string
 
 	var part []rune
-	var symAdj bool
+	var partGears []*gear
+	var partSymAdj bool
+
 	for yIdx, r := range s.rows {
 		for xIdx, v := range r {
-			if val(v).number() {
+			if val(v).num() {
 				part = append(part, v)
-				if s.symbolAdj(xIdx, yIdx) {
-					symAdj = true
+				if ok, g := s.symAdj(xIdx, yIdx); ok {
+					partSymAdj = true
+					partGears = append(partGears, g...)
 				}
 			}
 			if len(part) != 0 {
-				// Moving out of number
-				if !val(v).number() || xIdx == s.ubX {
-					if symAdj {
-						parts = append(parts, string(part))
+				if !val(v).num() || xIdx == s.ubX {
+					if partSymAdj {
+						p, err := strconv.Atoi(string(part))
+						if err != nil {
+							panic("handle me")
+						}
+
+						s.parts = append(s.parts, p)
+						for _, g := range partGears {
+							g.add(p)
+						}
 					}
 					part = nil
-					symAdj = false
+					partGears = nil
+					partSymAdj = false
 				}
 			}
 		}
 	}
 
-	return parts
+	return s
+}
+
+func (s *schematic) gear(x, y int) *gear {
+	k := fmt.Sprintf("%v,%v", x, y)
+
+	if s.at(x, y).gear() {
+		g := s.gears[k]
+		if g == nil {
+			g = &gear{nums: map[int]int{}}
+		}
+		s.gears[k] = g
+	}
+
+	return s.gears[k]
+}
+
+func (s schematic) symAdj(x, y int) (adj bool, gears []*gear) {
+	coords := [][]int{
+		{x - 1, y},
+		{x + 1, y},
+		{x, y - 1},
+		{x, y + 1},
+		{x + 1, y + 1},
+		{x + 1, y - 1},
+		{x - 1, y + 1},
+		{x - 1, y - 1},
+	}
+
+	for _, c := range coords {
+		if v := s.at(c[0], c[1]); v.sym() {
+			adj = true
+			if g := s.gear(c[0], c[1]); g != nil {
+				gears = append(gears, g)
+			}
+		}
+	}
+
+	return adj, gears
 }
 
 func (s schematic) at(x, y int) (v val) {
@@ -119,13 +161,20 @@ func (sl Solution) Solve() (string, error) {
 	s := newSchematic(rows)
 
 	var sum int
-	for _, p := range s.parts() {
-		v, err := strconv.Atoi(p)
-		if err != nil {
-			return "", fmt.Errorf("converting part: %w", err)
-		}
-		sum += v
+	for _, p := range s.parts {
+		sum += p
 	}
 
-	return fmt.Sprintf("Sum of parts: %v", sum), nil
+	var gears int
+	for _, g := range s.gears {
+		if len(g.nums) == 2 {
+			vals := []int{}
+			for n := range g.nums {
+				vals = append(vals, n)
+			}
+			gears += vals[0] * vals[1]
+		}
+	}
+
+	return fmt.Sprintf("Sum of parts: %v; gears: %v", sum, gears), nil
 }
